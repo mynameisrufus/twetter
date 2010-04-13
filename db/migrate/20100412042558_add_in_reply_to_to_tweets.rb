269f53ff7fd096705faaf8fd8e20d1d15d969d7c
Ben Hoskings <ben@hoskings.net>
2010-04-13 15:53:11 +1000

    Added logging to AddInReplyToToTweets.

--- db/migrate/20100412042558_add_in_reply_to_to_tweets.rb
+++ db/migrate/20100412042558_add_in_reply_to_to_tweets.rb
@@ -2,13 +2,20 @@ class AddInReplyToToTweets < ActiveRecord::Migration
   def self.up
     add_column :tweets, :in_reply_to_status_id, :integer
     raise unless Tweet.replies.all? {|tweet|
-      in_reply_to = User.find_by_id(tweet.recipient_id).tweets.find(
-        :first,
-        :conditions => ["created_at < ?", tweet.created_at],
-        :order => "created_at DESC"
-      )
-      unless tweet.nil?
-        tweet.update_attribute :in_reply_to_status_id, in_reply_to.id
+      user = User.find_by_id(tweet.recipient_id)
+      if user.nil?
+        puts "Missing User<#{tweet.recipient_id}> for Tweet<#{tweet.id}> (#{tweet.tweet})"
+      else
+        in_reply_to = user.tweets.find(
+          :first,
+          :conditions => ["created_at < ?", tweet.created_at],
+          :order => "created_at DESC"
+        )
+        if tweet.nil?
+          puts "Missing in_reply_to tweet for User<#{tweet.recipient_id}>"
+        else
+          tweet.update_attribute :in_reply_to_status_id, in_reply_to.id
+        end
       end
     }
   end
