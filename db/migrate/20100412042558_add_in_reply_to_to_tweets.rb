aac1d5ddd7f82ba94beddf61d4831ed62e6a31d7
Ben Hoskings <ben@hoskings.net>
2010-04-13 15:47:37 +1000

    Use replies named_scope instead of checking each tweet.

--- db/migrate/20100412042558_add_in_reply_to_to_tweets.rb
+++ db/migrate/20100412042558_add_in_reply_to_to_tweets.rb
@@ -1,18 +1,14 @@
 class AddInReplyToToTweets < ActiveRecord::Migration
   def self.up
     add_column :tweets, :in_reply_to_status_id, :integer
-    raise unless Tweet.all.all? {|tweet|
-      if tweet.type != 'reply'
-        true
-      else
-        in_reply_to = User.find_by_id(tweet.recipient_id).tweets.find(
-          :first,
-          :conditions => ["created_at < ?", tweet.created_at],
-          :order => "created_at DESC"
-        )
-        unless tweet.nil?
-          tweet.update_attribute :in_reply_to_status_id, in_reply_to.id
-        end
+    raise unless Tweet.replies.all? {|tweet|
+      in_reply_to = User.find_by_id(tweet.recipient_id).tweets.find(
+        :first,
+        :conditions => ["created_at < ?", tweet.created_at],
+        :order => "created_at DESC"
+      )
+      unless tweet.nil?
+        tweet.update_attribute :in_reply_to_status_id, in_reply_to.id
       end
     }
   end
