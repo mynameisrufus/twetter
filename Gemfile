2bd88945ef52b3063fa16bc7b498bd586c2b57f0
Ben Hoskings <ben@hoskings.net>
2012-01-14 19:27:02 +1100

    Add unicorn.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source :rubygems
 
 gem 'rails', '2.3.10'
+gem 'unicorn'
 
 gem 'hirb'
 gem 'paperclip'
---
+++ config/unicorn.rb
@@ -0,0 +1,27 @@
+worker_processes 4
+working_directory '.'
+
+preload_app true
+timeout 60
+
+listen "/Users/ben/code/twetter/tmp/sockets/unicorn.socket", :backlog => 64
+pid "/Users/ben/code/twetter/tmp/pids/unicorn.pid"
+stderr_path "/Users/ben/code/twetter/log/unicorn.stderr.log"
+stdout_path "/Users/ben/code/twetter/log/unicorn.stdout.log"
+
+before_fork do |server, worker|
+  begin
+    old_pid = File.read("#{server.pid}.oldbin").to_i
+    STDERR.puts "[worker #{worker.nr}] sending SIGQUIT to #{old_pid}"
+    Process.kill("QUIT", old_pid)
+  rescue Errno::ENOENT
+    STDERR.puts "[worker #{worker.nr}] no old master running."
+  rescue Errno::ESRCH
+    STDERR.puts "[worker #{worker.nr}] #{old_pid} was already gone."
+  end
+  ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end
