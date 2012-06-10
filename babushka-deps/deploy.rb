cd8e7b847c9f0e3946b0b62fa68f74ddc3e4cfdd
Ben Hoskings <ben@hoskings.net>
2012-06-10 18:32:58 +1000

    Remove the asset pipeline build on deploy -- it's disabled for now.

--- babushka-deps/deploy.rb
+++ babushka-deps/deploy.rb
@@ -1,10 +1,3 @@
 dep "on deploy", :old_id, :new_id, :branch, :env do
   requires 'benhoskings:when path changed'.with('db/migrate/', 'migrated db', old_id, new_id, env)
-  requires "assets compiled".with(env)
-end
-
-dep "assets compiled", :env, :template => "benhoskings:task" do
-  run {
-    shell "bundle exec rake assets:precompile RAILS_ENV=#{env}"
-  }
 end
