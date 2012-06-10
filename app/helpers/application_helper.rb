4dd5311898b44f898771ad23e40d66d91b0bd9e5
Ben Hoskings <ben@hoskings.net>
2012-06-10 19:59:15 +1000

    Remove some misguided promotions.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,18 +19,8 @@ module ApplicationHelper
 
   def promotions
     [
-      {:name => 'Rails&middot;camp', :part => 'n.', :text => 'Hermanos, hacking and hops. The good life.'},
-      {:name => 'Rails&middot;camp', :part => 'n.', :text => 'Brews, bros and brains. The good life.'},
-
-      {:name => 'La&middot;del', :part => 'n.', :text => 'Traditionally used for serving food, employed during the camp for powerful drinking.'},
-
       {:name => 'Cer&middot;ve&middot;za', :part => 'n.', :text => 'Ponerse al día con sus compañeros y disfrutando de unas cervezas es un orgullo la tradición celebrada Railscamp.'},
       {:name => 'Bier', :part => 'n.', :text => 'Aufholjagd mit Kumpels und genießen ein paar Bier ist eine stolz erhobenem Railscamp Tradition.'},
-
-      {:name => 'Mex&middot;i&middot;can', :part => 'n.', :text => "Don't ask; consume."},
-
-      {:name => 'Gui&middot;tar He&middot;ro', :part => 'n.', :text => 'Because not all of us can be Eric Johnson.'},
-
       {:name => 'Ba&middot;na&middot;na&middot;jour', :part => 'n.', :text => 'A sinatra/bonjour app by @toolmantim for sharing publishing and discovering git repos. n.b.: There are no secret bananas.'}
     ]
   end
